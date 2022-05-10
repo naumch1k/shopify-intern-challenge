1982c627660a6b638d05f7e7f1f9db66ac513f82
Irina Naumchik <irina.naumchik2012@yandex.ru>
2022-05-10 14:24:33 -0500

    feat: create and implement Header component

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,9 +1,12 @@
 import { Page } from '../page';
+import { Header } from '../header';
 
 export const App = () => {
   return (
     <Page>
-      <Page.Header/>
+      <Page.Header>
+        <Header/>
+      </Page.Header>
       <Page.Content/>
       <Page.Footer/>
     </Page>
