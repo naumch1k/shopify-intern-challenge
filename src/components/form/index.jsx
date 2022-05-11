9dff223764f619efc19d781afaf005f0b2593f8d
Irina Naumchik <irina.naumchik2012@yandex.ru>
2022-05-10 21:50:40 -0500

    feat: implement form validation

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,10 +1,28 @@
 import { Button } from '../button';
 import styles from './form.module.css';
+import useFormWithValidation from '../../hooks/useFormWithValidation';
 
 export const Form = () => {
+  const {
+    values,
+    errors,
+    isValid,
+    handleChange,
+    resetForm,
+  } = useFormWithValidation({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    resetForm();
+    // eslint-disable-next-line no-console
+    console.log('handleSubmit');
+  };
+
   return (
     <form
       name="prompt-form"
+      values={values}
+      onSubmit={handleSubmit}
     >
       <fieldset className={styles.fieldset}>
         <textarea
@@ -13,12 +31,16 @@ export const Form = () => {
           name="prompt-input"
           placeholder="Start typing here"
           required
+          onChange={handleChange}
         />
+        <span className={styles.error}>
+          {errors['prompt-input'] && errors['prompt-input']}
+        </span>
         <div className={styles.submitGroup}>
           <Button
             type="submit"
             text="Submit"
-            disabled
+            disabled={!isValid}
           />
         </div>
       </fieldset>
